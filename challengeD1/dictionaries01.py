63b572accd6443eaff88e4cd91070b00c1cd5a7b
Kathy Le <kathyle.dev@gmail.com>
2021-03-29 19:16:03 -0400

    fixed dictionaries01 code so that it fits all bonus challenge rules

--- challengeD1/dictionaries01.py
+++ challengeD1/dictionaries01.py
@@ -2,29 +2,37 @@
 
 # use the dictionary below to pull the appropriate value based on user's inputs
 
-heroes=  {
-"wolverine":
-    {"real name": "James Howlett",
-    "powers": "regeneration",
-    "archenemy": "Sabertooth",},
-"harry potter":
-    {"real name": "Harry Potter",
-    "powers": "he's a wizard",
-    "archenemy": "Voldemort",},
-"agent fitz":
-    {"real name": "Leopold Fitz",
-    "powers": "intelligence",
-    "archenemy": "Hydra",}
-        }
-### make sure user's input is error proof
+heroes = {
+    "wolverine":
+        {"real name": "James Howlett",
+         "powers": "regeneration",
+         "archenemy": "Sabertooth", },
+    "harry potter":
+        {"real name": "Harry Potter",
+         "powers": "he's a wizard",
+         "archenemy": "Voldemort", },
+    "agent fitz":
+        {"real name": "Leopold Fitz",
+         "powers": "intelligence",
+         "archenemy": "Hydra", }
+}
+### make sure user's input is error proof & able to loop through the prompts by putting it in a function
 
-#save a user's input to variable "char_name"
-hero_names = heroes.keys()
-char_name = ""
-while !hero_names__contains__(char_name):
-    char_name = input(" Which character do you want to know about? (Wolverine, Harry Potter, Agent Fitz) ")
 
-# save a user's input to variable "char_stat"
-char_stat = input("What statistic do you want to know about? (real name, powers, archenemy) ")
+def get_user_input():
+    # save a user's input to variable "char_name" & char_stat
+    char_name = ""
+    char_stat = ""
+    while not (char_name in heroes):
+        char_name = input(" Which character do you want to know about? (Wolverine, Harry Potter, Agent Fitz) ").lower()
+    while not (char_stat in heroes[char_name]):
+        char_stat = input("What statistic do you want to know about? (real name, powers, archenemy) ").lower()
+    #print hero's info based on user's input
+    print(f"{char_name.capitalize()}'s {char_stat} is = " + heroes[char_name][char_stat])
 
-print(f"{char_name.capitalize()}'s {char_stat} is = " + heroes[char_name.lower()][char_stat.lower()])
+
+# create a variable that will hold input for user to exit program
+user_exit = ""
+while not (user_exit == "q"):
+    get_user_input()
+    user_exit = input("Press 'q' to quit. Press any other key to try again ").lower()
