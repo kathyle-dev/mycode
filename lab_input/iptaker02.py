013a71b2d92661a25e8ed48b463c1235884619f7
Kathy Le <kathyle.dev@gmail.com>
2021-03-29 17:47:28 +0000

    adding Lab8 files of practice with input()

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+user_input = input("Please enter an IPv4 IP adress: ")
+
+print("You told me the IPv4 adress is: ", user_input)
+
+vendor_name = input("Please enter the vendor name associated with this device:")
+
+print("You told me the vendor name is: ", vendor_name)
