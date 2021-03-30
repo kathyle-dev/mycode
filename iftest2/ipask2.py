b7ab54d3c57d8c41d46e4c31e5048eeff3781a05
Kathy Le <kathyle.dev@gmail.com>
2021-03-30 21:16:02 +0000

    adding challenge fix for lab18

--- iftest2/ipask2.py
+++ iftest2/ipask2.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
+import ipaddress
 ipchk = input("Apply an IP address: ") # this line now prompts the user for input
-
-# if user set IP of gateway
+#helper function to check if input is a valid ip address
+def validate_address(ipchk):
+    try:
+        ipaddress.ip_address(ipchk)
+    except ValueError:
+        print("You did not provide proper input.")
+    else:
+        print("Looks like the IP address was set: " + ipchk)
+## if user set IP of gateway
+# IF it matches the gateway, immediately print the following ELSE, must check f it is a valid input
 if ipchk == "192.168.70.1":
    print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
-elif ipchk: # if any data is provided, this will test true
-   print("Looks like the IP address was set: " + ipchk) # indented under if
-else: # if data is NOT provided
-   print("You did not provide input.") # indented under else
- 
+else:
+    validate_address(ipchk)
+
+
+# print exit string
+print("exiting program")
