824ea6ccc15ca7abf219551663374dfbea9ac6fc
Kathy Le <kathyle.dev@gmail.com>
2021-04-01 20:54:24 +0000

    lab37 - working with pandas

---
+++ excelout/excelwork.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+## sudo apt install python3-pip
+
+## python3 -m pip install pyexcel
+## python3 -m pip install pyexcel-xls
+import pyexcel
+
+# Request data from user
+def get_user_data():
+    input_word = input("\nWhat is your favorite curse word? ")
+    input_food = input("What is your favorite food? ")
+    input_color = input("what is your favorite color? ")
+    input_show = input("What's the best tv show of all time? ")
+    d = {"Word": input_word, "Food": input_food, "Color": input_color, "Show": input_show}
+    return d
+
+## This code is left turned off, but might help visualize how pyexcel works with data sets.
+## IP is the first column, whereas driver is the second column.
+## mylistdict = [ {"IP": "172.16.2.10", "driver": "arista_eos"}] {"IP": "172.16.2.20", "driver": "arista_eos"} ]
+## pyexcel.save_as(records=mylistdict, dest_file_name="ip_list.xls")
+
+
+# Runtime
+mylistdict = []  # this will be our list we turn into a *.xls file
+
+print("Hello! This program will make you a *.xls file")
+
+while(True):
+    mylistdict.append(get_user_data())
+    keep_going = input("\nWould you like to add another value? Enter to continue, or \
+    enter 'q' to quit: ")
+    if (keep_going.lower() == 'q'):
+        break
+
+filename = input("\nWhat is the name of the *.xls file? ")
+
+pyexcel.save_as(records=mylistdict, dest_file_name=f'{filename}.xls')
+
+print("The file " + filename + ".xls should be in your local directory")
+
