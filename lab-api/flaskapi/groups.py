a3b745dccf9d26fc161848d3f9eb0a3ed57c7577
Kathy Le <kathyle.dev@gmail.com>
2021-04-08 15:28:26 -0400

    Flask Challenge

---
+++ lab-api/flaskapi/groups.py
@@ -0,0 +1,3 @@
+group_list = [{"hostname": "hostA", "ip": "192.168.30.22", "fqdn": "hostA.localdomain"},
+              {"hostname": "hostB", "ip": "192.168.30.33", "fqdn": "hostB.localdomain"},
+              {"hostname": "hostC", "ip": "192.168.30.44", "fqdn": "hostC.localdomain"}]
